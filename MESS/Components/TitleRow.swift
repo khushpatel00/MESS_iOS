6b208ff41349c6c770bdf7835a16ff7d4ac5e535
khushpatel00 <patelxkhush@gmail.com>
2026-04-30 11:05:11 +0530

    Added: Profile Row (MESS/Components/TitleRow.swift) for DM

---
+++ MESS/Components/TitleRow.swift
@@ -0,0 +1,50 @@
+//
+//  TitleRow.swift
+//  MESS
+//
+//  Created by khush on 28/04/2026.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+
+    var userName = "Sam Porter"
+    
+    var body: some View {
+        HStack(spacing: 20){
+            Image("ProfileSamPorter")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 50, height: 50)
+                .clipShape(.circle)
+            VStack(alignment: .leading){
+                Text(userName)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                
+                Text("Online")
+                    .font(.caption)
+                    .foregroundStyle(.bar)
+            }
+            Spacer()
+            Image(systemName: "phone.fill")
+                .foregroundStyle(.gray)
+                .clipShape(.circle)
+//                .frame(width: 50, height: 50)
+                .padding(10)
+                .background(.white)
+                .clipShape(.circle)
+                
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        
+    }
+}
+
+#Preview {
+    TitleRow()
+        .background(Color(.brown))
+}
